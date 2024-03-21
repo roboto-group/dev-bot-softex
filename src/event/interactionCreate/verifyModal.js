ffaa34b0528eabfe7628fefaa00e57e6d3b068e7
Ezequiel-leandro-jr <ezequiel.leandro.jr@hotmail.com>
2024-03-21 16:25:59 -0300

    starting construction of the modal

--- src/event/interactionCreate/verifyModal.js
+++ src/event/interactionCreate/verifyModal.js
@@ -1,1 +1,27 @@
-const { Client } = require('discord.js');
+const { Client, Interaction } = require('discord.js');
+
+/**
+   * 
+   * @param {Client} client 
+   * @param {Interaction} interaction 
+   */
+
+module.exports = (client, interaction) => {
+
+    if(!interaction.isButton()) return;
+    interaction.deferReply({ ephemeral: true });
+
+    const button = client.channels.cache.get(interaction.customId);
+
+    if(!button) {
+        interaction.reply({
+            content: 'Elemento não encontrado.',
+        });
+        return;
+    }
+
+    
+}
+
+
+
