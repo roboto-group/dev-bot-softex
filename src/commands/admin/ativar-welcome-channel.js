b7f1b195abe3a79fb583b1b767df00c4ea9d847d
Ezequiel-leandro-jr <ezequiel.leandro.jr@hotmail.com>
2024-03-30 15:10:25 -0300

    setup welcome channel configure

---
+++ src/commands/admin/ativar-welcome-channel.js
@@ -0,0 +1,60 @@
+const { ApplicationCommandOptionType, SlashCommandBuilder, Interaction, PermissionFlagsBits, ChannelType } = require('discord.js');
+const WelcomeChannelSchema = require('../../models/WelcomeChannel');
+
+module.exports = {
+  /**
+   * 
+   * @param {Interaction} interaction 
+   */
+  callback: async (client, interaction) => {
+    //Checando se o comando foi executado em um servidor
+    if (!interaction.inGuild()) {
+        interaction.reply({
+          content: 'Este comando só pode ser executado de um servidor.'
+        })
+        return;
+      }
+
+    const targetChannel = interaction.options.getChannel('target-channel');
+      
+    try {
+      await interaction.deferReply({ephemeral: true});
+
+      var welcomeChannel = await WelcomeChannelSchema.findOne({ guildId: interaction.guild.id, channelId: targetChannel.id});
+
+      if (welcomeChannel) {
+        //Checando no BD se há um registro com esse mesmo canal
+        if (welcomeChannel.channelId === targetChannel.id) {
+            interaction.editReply(`O canal ${targetChannel} já está configurado no modo 'Boas-Vindas'. Para desabilitar a função, execute "/desativar-welcome-channel"`);
+            return;
+          }
+      } else {
+         welcomeChannel = new WelcomeChannelSchema({
+            guildId: interaction.guild.id,
+            channelId: targetChannel.id,
+        });
+      }
+      //Salvando alterações no BD
+      await welcomeChannel.save();
+      //Respondendo ao usuário
+      interaction.editReply(`Canal ${targetChannel} configurado no modo 'Boas-Vindas' com sucesso! Para desabilitar a função, execute "/desativar-welcome-channel"`);
+
+    } catch (error) {
+        console.log(`Erro na execução do comando:\n${error}`);
+    }
+
+  },
+  name: 'ativar-welcome-channel',
+  description: 'Configura um canal de boas-vindas para este servidor.',
+  devOnly: true,
+  options: [
+    {
+      name: 'target-channel',
+      description: 'O canal que você quer que sirva de boas-vindas neste servidor.',
+      type: ApplicationCommandOptionType.Channel,
+      required: true,
+    }
+  ],
+  permissionsRequired: [PermissionFlagsBits.Administrator],
+
+}
---
+++ src/models/WelcomeChannel.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const welcomeChannelSchema = new Schema({
+    guildId: {
+        type: String,
+        required: true,
+        //unique: true, //limita a apenas um canal do servidor
+    },
+    channelId: {
+        type: String,
+        required: true,
+        unique: true,
+    }
+}, 
+{ timestamps: true }
+);
+
+module.exports = model('WelcomeChannel', welcomeChannelSchema);
