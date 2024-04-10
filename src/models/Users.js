c6884fef5c0d1f8fccc4687a3003ee111b64a6ba
Ezequiel-leandro-jr <ezequiel.leandro.jr@hotmail.com>
2024-04-09 23:35:15 -0300

    update User model email

--- src/models/Users.js
+++ src/models/Users.js
@@ -33,6 +33,10 @@ const userSchema = new Schema({
     type: Date,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
 })
 
 module.exports = model('User', userSchema);
